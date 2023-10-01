845f3dc771e319664a442393a9ef2892d37b1459
ZbWeR <1641064392@qq.com>
2023-10-02 00:36:04 +0800

    🔧 build: rollup 配置文件 & 构建脚本

---
+++ rollup.config.js
@@ -0,0 +1,75 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+import replace from "@rollup/plugin-replace";
+import terser from "@rollup/plugin-terser";
+
+const __filename = fileURLToPath(import.meta.url);
+const ROOT_PATH = path.dirname(__filename);
+const resolve = (p) => path.resolve(ROOT_PATH, p);
+const name = "zue";
+
+const outputConfigs = {
+  cjs: {
+    file: resolve(`dist/${name}.cjs.js`),
+    format: `cjs`,
+  },
+  global: {
+    file: resolve(`dist/${name}.global.js`),
+    format: `iife`,
+    name: "zue",
+  },
+  "esm-bundler": {
+    file: resolve(`dist/${name}.esm-bundler.js`),
+    format: `esm`,
+  },
+  "esm-browser": {
+    file: resolve(`dist/${name}.esm-browser.js`),
+    format: `esm`,
+  },
+};
+
+const rollupConfigs = Object.keys(outputConfigs).map((format) =>
+  createConfig(format, outputConfigs[format])
+);
+
+function createConfig(format, output) {
+  const isProductionBuild = process.env.__DEV__ === "false";
+  const isBundlerESMBuild = /esm-bundler/.test(format);
+
+  // 入口文件配置
+  let entryFile = resolve("src/index.js");
+  // 导出文件名调整
+  if (isProductionBuild) output.file = output.file.replace(".js", ".prod.js");
+  // replace 插件配置
+  const replacements = {
+    __TEST__: `false`,
+    // esm-bundler 中 __DEV__ 不能直接替换为常量.
+    __DEV__: isBundlerESMBuild
+      ? `!!(process.env.NODE_ENV !== 'production')`
+      : String(!isProductionBuild),
+  };
+  const plugins = [replace({ values: replacements, preventAssignment: true })];
+  // terser 插件配置
+  if (isProductionBuild) {
+    plugins.push(
+      terser({
+        compress: { arguments: true, dead_code: true },
+        toplevel: true,
+        keep_classnames: true,
+        keep_fnames: true,
+      })
+    );
+  }
+  return {
+    input: entryFile,
+    output: {
+      ...output,
+      // 开发环境默认开启 sourcemap
+      sourcemap: !!process.env.SOURCE_MAP,
+    },
+    plugins,
+  };
+}
+
+export default rollupConfigs;
---
+++ scripts/build.js
@@ -0,0 +1,63 @@
+import fs from "node:fs";
+import minimist from "minimist";
+import chalk from "chalk";
+import { execa } from "execa";
+
+const argv = minimist(process.argv.slice(2));
+const devOnly = argv.devOnly || argv.d;
+const sourceMap = argv.sourceMap || argv.s;
+
+build();
+
+/**
+ * 向 rollup 传递环境变量
+ */
+async function build() {
+  try {
+    let startTime = new Date();
+
+    const { stderr } = await execa(
+      "rollup",
+      [
+        "-c",
+        "--environment",
+        [
+          `NODE_ENV:${devOnly ? "development" : "production"}`,
+          `__DEV__:${devOnly ? "true" : "false"}`,
+          sourceMap ? `SOURCE_MAP:true` : ``,
+        ]
+          .filter(Boolean)
+          .join(","),
+      ],
+      { stdio: "pipe" }
+    );
+    // 输出构建相关信息
+    await logTimeAndSize(stderr);
+
+    let costTime = new Date() - startTime;
+    console.log(chalk.bgGreenBright(`\n构建用时：${costTime} ms`));
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function logTimeAndSize(stdout) {
+  // 剔除颜色转义字符
+  const str = stdout.replace(/\u001b\[\d+m/g, "");
+  // 捕获文件名称与构建耗时
+  const fileRegex = /created\s(.*?)\sin\s(\d+)ms/g;
+  const matches = Array.from(str.matchAll(fileRegex), (match) => [
+    match[1],
+    match[2],
+  ]);
+
+  for (let match of matches) {
+    const [filePath, costTime] = match;
+    // 读取文件获得大小
+    const { size } = await fs.promises.stat(filePath);
+    const tmpStr = `Size: ${chalk.cyan(
+      (size / 1024).toFixed(2) + "kb"
+    )}, Time: ${chalk.cyan(costTime + "ms")}, File: ${chalk.cyan(filePath)}`;
+    console.log(chalk.green(tmpStr));
+  }
+}
