f32677f6bf95e3e6e87a1fc936d19cd9a659b553
ZbWeR <1641064392@qq.com>
2023-10-29 23:50:18 +0800

    🐎 ci: 增加路径别名

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from "vitest/config";
+import { resolve } from "path";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      types: resolve("src/types"),
+      compiler: resolve("src/compiler"),
+      reactivity: resolve("src/reactivity"),
+      renderer: resolve("src/renderer"),
+    },
+  },
   define: {
     __DEV__: true,
   },
@@ -9,9 +18,6 @@ export default defineConfig({
     coverage: {
       reporter: ["html"],
       enabled: true,
-    },
-    alias: {
-      types: "/src/types",
     },
   },
 });
